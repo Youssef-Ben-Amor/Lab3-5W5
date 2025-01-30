2ef67f6516f96b03a3b52e2ba0ebda627926b59f
Youssef Ben Amor <2375637@cegepmontpetit.ca>
2025-01-30 14:43:53 -0500

    Fct  : avancement

---
+++ Migrations/20250130193801_MigMig1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RelationsNaN.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigMig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlatformsId",
+                table: "Game",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlatformsId",
+                table: "Game");
+        }
+    }
+}
